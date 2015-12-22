312d370068b07df22af0b0d5c0953b6a57ee1c7e
Kapil Paliwal <kapil.paliwal1@wipro.com>
2015-12-22 17:48:28 +0530

    ignoring insert and update for DQ and MQ

--- md-dao/src/test/java/com/wipro/ats/bdre/md/dao/DataQualityDAOTest.java
+++ md-dao/src/test/java/com/wipro/ats/bdre/md/dao/DataQualityDAOTest.java
@@ -20,6 +20,7 @@ import com.wipro.ats.bdre.md.beans.DQSetupInfo;
 import com.wipro.ats.bdre.md.beans.table.Properties;
 import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -43,7 +44,7 @@ public class DataQualityDAOTest {
 
     @Autowired
     DataQualityDAO dataQualityDAO;
-
+    @Ignore
     @Test
     public void InsertUpdateTest() {
         DQSetupInfo dqSetupInfo = new DQSetupInfo();
