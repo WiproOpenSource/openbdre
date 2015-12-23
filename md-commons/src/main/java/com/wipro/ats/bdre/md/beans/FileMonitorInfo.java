126b3504c891be59b576c1e84eef99a3a5624d75
Prem Kumar <prem.lumar51@wipro.com>
2015-12-23 18:56:23 +0530

    added file monitor

--- md-commons/src/main/java/com/wipro/ats/bdre/md/beans/FileMonitorInfo.java
+++ md-commons/src/main/java/com/wipro/ats/bdre/md/beans/FileMonitorInfo.java
@@ -1,20 +1,29 @@
 package com.wipro.ats.bdre.md.beans;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * Created by PR324290 on 12/22/2015.
  */
 public class FileMonitorInfo {
     @NotNull
+    @Size(min = 1, max = 45)
     private String monitoredDirName;
     @NotNull
+    @Size(min = 1, max = 45)
     private String filePattern;
     @NotNull
+    @Size(min = 1, max = 45)
     private String deleteCopiedSource;
     @NotNull
+    @Size(min = 1, max = 45)
     private String hdfsUploadDir;
     @NotNull
+    @Min(value = 100)
+    @Max(value = 10000)
     private int sleepTime;
     public String getMonitoredDirName() {
         return monitoredDirName;
