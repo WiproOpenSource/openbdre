dc4967a2800aba0704341cfd7c0c223dd4549793
Arijit Banerjee <arijit_banerjee1@wipro.com>
2016-01-05 00:12:16 +0530

    fixing run ui

--- bdre-scripts/local/execution/run-ui.sh
+++ bdre-scripts/local/execution/run-ui.sh
@@ -4,4 +4,4 @@
 BDRE_HOME=~/bdre
 BDRE_APPS_HOME=~/bdre_apps
 
-java -jar  $BDRE_HOME/lib/webapps/jetty-runner.jar --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war
+java -jar  $BDRE_HOME/lib/webapps/jetty-runner.jar --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui-$bdreVersion.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war
