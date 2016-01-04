aad071ab1dd26e86039206f184455527412a3ce1
Ashutosh Kumar Rai <ashutosh.rai4@wipro.com>
2016-01-04 15:32:12 +0530

    FlumePropertiesAPI changes and removing unused imports from CrawlerAPI

--- md-rest-api/src/main/java/com/wipro/ats/bdre/md/rest/ext/FlumePropertiesAPI.java
+++ md-rest-api/src/main/java/com/wipro/ats/bdre/md/rest/ext/FlumePropertiesAPI.java
@@ -25,6 +25,7 @@ import com.wipro.ats.bdre.md.dao.jpa.ProcessTemplate;
 import com.wipro.ats.bdre.md.dao.jpa.PropertiesId;
 import com.wipro.ats.bdre.md.dao.jpa.WorkflowType;
 import com.wipro.ats.bdre.md.rest.RestWrapper;
+import com.wipro.ats.bdre.md.rest.util.Dao2TableUtil;
 import com.wipro.ats.bdre.md.rest.util.DateConverter;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,11 +60,15 @@ public class FlumePropertiesAPI extends MetadataAPIBase {
     RestWrapper list(@RequestParam Map<String, String> map, Principal principal) {
         LOGGER.debug(" value of map is " + map.size());
         RestWrapper restWrapper = null;
-        com.wipro.ats.bdre.md.beans.table.Process parentProcess = new Process();
+
+        /* com.wipro.ats.bdre.md.beans.table.Process parentProcess = new Process();
         Process childProcess = new Process();
         parentProcess = insertProcess(1, null, "flume action Parent", "Data Ingestion Parent", 2, principal);
-        childProcess = insertProcess(23, parentProcess.getProcessId(), "flume action", "flume action", 0, principal);
-
+        childProcess = insertProcess(23, parentProcess.getProcessId(), "flume action", "flume action", 0, principal);  */
+        com.wipro.ats.bdre.md.dao.jpa.Process parentProcess = Dao2TableUtil.buildJPAProcess(1, "flume action Parent", "Data Ingestion Parent", 2);
+        com.wipro.ats.bdre.md.dao.jpa.Process childProcess = Dao2TableUtil.buildJPAProcess(23, "child of " + "flume action", "child of " + "flume action", 0);
+        List<com.wipro.ats.bdre.md.dao.jpa.Properties> childProps=new ArrayList<>();
+        com.wipro.ats.bdre.md.dao.jpa.Properties jpaProperties;
 
         for (String string : map.keySet()) {
             LOGGER.debug("String is" + string);
@@ -74,122 +79,48 @@ public class FlumePropertiesAPI extends MetadataAPIBase {
             String key = string.substring(splitIndex + 1, string.length());
             LOGGER.debug("key is " + key);
             if (string.startsWith("source_")) {
-                insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sources.source" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for source");
+                jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".sources.source" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for source");
+                childProps.add(jpaProperties);
+//                insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sources.source" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for source");
             } else if (string.startsWith("channel_")) {
-                insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".channels.channel" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for channel");
+                jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".channels.channel" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for channel");
+                childProps.add(jpaProperties);
+//                insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".channels.channel" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for channel");
             } else if (string.startsWith("sink_")) {
-                insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for sink");
+                jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for sink");
+                childProps.add(jpaProperties);
+//                insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + "." + key, map.get(string), "Properties for sink");
             }
         }
-        insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sources", "source" + childProcess.getProcessId(), "Source name");
+
+        jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".sources", "source" + childProcess.getProcessId(), "Source name");
+        childProps.add(jpaProperties);
+        jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".sinks", "sink" + childProcess.getProcessId(), "Sink name");
+        childProps.add(jpaProperties);
+        jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(),"flume",  "agent" + parentProcess.getProcessId() + ".channels", "channel" + childProcess.getProcessId(), "Channel name");
+        childProps.add(jpaProperties);
+        jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".sources.source" + childProcess.getProcessId() + ".channels", "channel" + childProcess.getProcessId(), "Channel name for source");
+        childProps.add(jpaProperties);
+        jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(), "flume", "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + ".channel", "channel" + childProcess.getProcessId(), "Channel name for sink");
+        childProps.add(jpaProperties);
+        jpaProperties = Dao2TableUtil.buildJPAProperties(childProcess.getProcessId(),"flume",  "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + ".hdfs.processId", parentProcess.getProcessId().toString(), " Parent Process Id");
+        childProps.add(jpaProperties);
+
+        /* insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sources", "source" + childProcess.getProcessId(), "Source name");
         insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sinks", "sink" + childProcess.getProcessId(), "Sink name");
         insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".channels", "channel" + childProcess.getProcessId(), "Channel name");
         insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sources.source" + childProcess.getProcessId() + ".channels", "channel" + childProcess.getProcessId(), "Channel name for source");
         insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + ".channel", "channel" + childProcess.getProcessId(), "Channel name for sink");
-        insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + ".hdfs.processId", parentProcess.getProcessId().toString(), " Parent Process Id");
+        insertProperties(childProcess.getProcessId(), "agent" + parentProcess.getProcessId() + ".sinks.sink" + childProcess.getProcessId() + ".hdfs.processId", parentProcess.getProcessId().toString(), " Parent Process Id");  */
 
-
-        List<Process> processList = new ArrayList<Process>();
-        parentProcess.setCounter(2);
-        childProcess.setCounter(2);
-        processList.add(parentProcess);
-        processList.add(childProcess);
-        restWrapper = new RestWrapper(processList, RestWrapper.OK);
+        List<com.wipro.ats.bdre.md.dao.jpa.Process> processList = processDAO.createOneChildJob(parentProcess,childProcess,null,childProps);
+        List<Process> tableProcessList = Dao2TableUtil.jpaList2TableProcessList(processList);
+        Integer counter = tableProcessList.size();
+        for (Process process:tableProcessList) {
+            process.setCounter(counter);
+        }
+        restWrapper = new RestWrapper(tableProcessList, RestWrapper.OK);
         return restWrapper;
-    }
-
-    private Process insertProcess(Integer ptId, Integer ppId, String name, String desc, Integer wfId, Principal principal) {
-        Process process = new Process();
-        com.wipro.ats.bdre.md.dao.jpa.Process insertDaoProcess = new com.wipro.ats.bdre.md.dao.jpa.Process();
-        com.wipro.ats.bdre.md.dao.jpa.ProcessType daoProcessType = new com.wipro.ats.bdre.md.dao.jpa.ProcessType();
-        daoProcessType.setProcessTypeId(ptId);
-        insertDaoProcess.setProcessType(daoProcessType);
-        if (wfId != null) {
-            WorkflowType daoWorkflowType = new WorkflowType();
-            daoWorkflowType.setWorkflowId(wfId);
-            insertDaoProcess.setWorkflowType(daoWorkflowType);
-        }
-        BusDomain daoBusDomain = new BusDomain();
-        daoBusDomain.setBusDomainId(1);
-        insertDaoProcess.setBusDomain(daoBusDomain);
-        ProcessTemplate daoProcessTemplate = new ProcessTemplate();
-        daoProcessTemplate.setProcessTemplateId(0);
-        insertDaoProcess.setProcessTemplate(daoProcessTemplate);
-
-        if (ppId != null) {
-            com.wipro.ats.bdre.md.dao.jpa.Process parentProcess = new com.wipro.ats.bdre.md.dao.jpa.Process();
-            parentProcess.setProcessId(ppId);
-            insertDaoProcess.setProcess(parentProcess);
-            insertDaoProcess.setNextProcessId(ppId.toString());
-
-        } else {
-            insertDaoProcess.setNextProcessId("0");
-
-        }
-        insertDaoProcess.setDeleteFlag(false);
-        insertDaoProcess.setDescription(desc);
-        insertDaoProcess.setProcessName(name);
-        insertDaoProcess.setCanRecover(true);
-        insertDaoProcess.setEnqueuingProcessId(0);
-        insertDaoProcess.setAddTs(DateConverter.stringToDate(process.getTableAddTS()));
-        process.setDeleteFlag(false);
-        process.setBusDomainId(1);
-        process.setProcessTypeId(ptId);
-        process.setDescription(name);
-        process.setParentProcessId(ppId);
-        process.setNextProcessIds("0");
-        process.setProcessName(desc);
-        process.setWorkflowId(wfId);
-        process.setEnqProcessId(0);
-        process.setAddTS(DateConverter.stringToDate(process.getTableAddTS()));
-        process.setDeleteFlag(false);
-        process.setCanRecover(true);
-        try {
-            LOGGER.debug("Process" + name + " is going to be inserted " + process.getProcessTypeId());
-//            process = s.selectOne("call_procedures.InsertProcess", process);
-            Integer processId = processDAO.insert(insertDaoProcess);
-            process.setProcessId(processId);
-            process.setTableAddTS(DateConverter.dateToString(insertDaoProcess.getAddTs()));
-            process.setTableEditTS(DateConverter.dateToString(insertDaoProcess.getEditTs()));
-            process.setTableAddTS(DateConverter.dateToString(process.getAddTS()));
-            LOGGER.info("Record with ID:" + process.getProcessId() + " inserted in Process by User:" + principal.getName() + process);
-        } catch (Exception e) {
-            LOGGER.debug("Error Occurred");
-        }
-        return process;
-    }
-
-    private void insertProperties(Integer pid, String key, String value, String desc) {
-        try {
-
-            Properties properties = new Properties();
-            properties.setProcessId(pid);
-            properties.setConfigGroup("flume");
-            properties.setKey(key);
-            properties.setValue(value);
-            properties.setDescription(desc);
-
-            com.wipro.ats.bdre.md.dao.jpa.Properties insertProperties = new com.wipro.ats.bdre.md.dao.jpa.Properties();
-            PropertiesId propertiesId = new PropertiesId();
-            propertiesId.setPropKey(properties.getKey());
-            propertiesId.setProcessId(properties.getProcessId());
-            insertProperties.setId(propertiesId);
-            com.wipro.ats.bdre.md.dao.jpa.Process process = new com.wipro.ats.bdre.md.dao.jpa.Process();
-            process.setProcessId(properties.getProcessId());
-            insertProperties.setProcess(process);
-            insertProperties.setConfigGroup(properties.getConfigGroup());
-            insertProperties.setPropValue(properties.getValue());
-            insertProperties.setDescription(properties.getDescription());
-            propertiesDAO.insert(insertProperties);
-
-
-//            s.selectOne("call_procedures.InsertProperties", properties);
-
-
-        } catch (Exception e) {
-            LOGGER.debug("Error Occurred");
-        }
-
     }
 
     @RequestMapping(value = {"/", ""}, method = RequestMethod.PUT)
