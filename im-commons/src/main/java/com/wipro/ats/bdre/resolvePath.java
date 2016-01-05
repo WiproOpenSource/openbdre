4991cca6d61d51e3c8475548028acdb5921c32b2
Ashutosh Kumar Rai <ashutosh.rai4@wipro.com>
2016-01-05 18:27:45 +0530

    resolved pth fn working

--- im-commons/src/main/java/com/wipro/ats/bdre/resolvePath.java
+++ im-commons/src/main/java/com/wipro/ats/bdre/resolvePath.java
@@ -26,7 +26,7 @@ public class resolvePath {
     }
 
     public static void main (String par[]) {
-        findMatches("/hoem/alkjsbg2347/%ksj/%/djfh%Hs/%H/%Hr/%HH/%ss/%s/%m/%M");
+        findMatches("/hoem/alkjsbg2347/%ksj/%/%H/%HH/%ss/%s/%m/%M");
     }
 
     public static void findMatches(String html){
@@ -35,17 +35,48 @@ public class resolvePath {
         System.out.println("------------------------");
         System.out.println("Following Matches found:");
         int count = 0;
+        StringBuffer htmlCopy = new StringBuffer(html);
+        Integer Indexes[]=new Integer[50];
+        Integer Values[]=new Integer[50];
+        Integer counter=0;
+        Integer value=null;
         while (matcher.find()) {
-            System.out.print("Start index: " + matcher.start());
-            System.out.print(" End index: " + matcher.end() + " ");
+            //System.out.print("Start index: " + matcher.start());
+            //System.out.print(" End index: " + matcher.end() + " ");
+            //System.out.println("yo " + replaceMatches(matcher, html));
+            value=Integer.parseInt(replaceMatches(matcher,matcher.group()).substring(1));
+
+            if(value!=200) {
+                System.out.println(value);
+                Indexes[counter] = matcher.end();
+                LOGGER.debug("123"+Indexes[counter]);
+                Values[counter++] = value;
+            }
             //System.out.println(matcher.group());
-            System.out.println(matcher.group());
+            //System.out.println(matcher.group());
             //replaceMatches(matcher,matcher.group());
             //if(count++ == 3)
                // break;
         }
-        System.out.println(matcher);
-        System.out.println("------------------------");
+        for (int i=0; i < counter;i++) {
+            //LOGGER.debug(htmlCopy.charAt(Indexes[i]-1)+" "+htmlCopy.charAt(Indexes[i]-2));
+            htmlCopy.deleteCharAt(Indexes[i] - 2);
+            htmlCopy.deleteCharAt(Indexes[i] - 2);
+            htmlCopy.insert(Indexes[i]-2,Values[i].toString());
+            //break;
+        }
+        LOGGER.debug("origstr "+html);
+        LOGGER.debug("mod str "+htmlCopy);
+        /*String replacer
+        char replacer;
+        for(int i=0;i<counter;i++)
+        {
+          System.out.println((htmlCopy.charAt(Indexes[i]-1)));
+
+        }*/
+
+       // System.out.println(matcher);
+       // System.out.println("------------------------");
     }
 
     /*public static String replaceMatches(String html){
@@ -55,16 +86,32 @@ public class resolvePath {
         return html;
     }*/
 
-    private static Matcher replaceMatches(Matcher matcher, String toBeMatched) {
+    private static String replaceMatches(Matcher matcher, String toBeMatched) {
         SimpleDateFormat sdf;
+        String timeReturn="-1";
         switch(toBeMatched) {
-            case "H":   sdf = new SimpleDateFormat(toBeMatched);
-                        System.out.println(matcher.group().replace(toBeMatched,sdf.format(new Date())));
-                        return matcher;
-            case "s":   sdf = new SimpleDateFormat(toBeMatched);
-                        System.out.println(matcher.replaceAll(sdf.format(new Date())));
-                        return matcher;
-            default:    return matcher;
+            case "%H":   sdf = new SimpleDateFormat(toBeMatched);
+                        //System.out.println(matcher.group().replace(toBeMatched,sdf.format(new Date())));
+                        timeReturn=sdf.format(new Date());
+                        System.out.println("yoo"+timeReturn);
+                        return timeReturn;
+            case "%s":   sdf = new SimpleDateFormat(toBeMatched);
+                       // System.out.println(matcher.replaceAll(sdf.format(new Date())));
+                        timeReturn=sdf.format(new Date());
+                        System.out.println("yoo"+timeReturn);
+                        return timeReturn;
+            case "%m":   sdf=new SimpleDateFormat(toBeMatched);
+                        //System.out.println(matcher.group().replace(toBeMatched,sdf.format(new Date())));
+                        timeReturn=sdf.format(new Date());
+                        System.out.println("yoo"+timeReturn);
+                        return timeReturn;
+            case "%M":   sdf=new SimpleDateFormat(toBeMatched);
+                //System.out.println(matcher.group().replace(toBeMatched,sdf.format(new Date())));
+                timeReturn=sdf.format(new Date());
+                System.out.println("yoo"+timeReturn);
+                return timeReturn;
+
+            default:    return "-200";
         }
     }
 }
