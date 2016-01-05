aeff731a2742d3de37f6a14a27bcd9a8373d16ef
Arijit Banerjee <arijit_banerjee1@wipro.com>
2016-01-05 23:54:33 +0530

    printing in test class

--- im-commons/src/test/java/com/wipro/ats/bdre/ResolvePathTest.java
+++ im-commons/src/test/java/com/wipro/ats/bdre/ResolvePathTest.java
@@ -14,15 +14,19 @@
 
 package com.wipro.ats.bdre;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ResolvePathTest {
-
+    private static Logger LOGGER = Logger.getLogger(ResolvePathTest.class);
     @Test
     public void testReplaceVars() throws Exception {
-        String resolved = ResolvePath.replaceVars("t=%t a=%a A=%A b=%b B=%B c=%c d=%d e=%e D=%D H=%H I=%I j=%j k=%k m=%m n=%n M=%M p=%p s=%s S=%S y=%y Y=%Y z=%z");
-        assert (!resolved.contains("Unknown"));
+        String input = "t=%t a=%a A=%A b=%b B=%B c=%c d=%d e=%e D=%D H=%H I=%I j=%j k=%k m=%m n=%n M=%M p=%p s=%s S=%S y=%y Y=%Y z=%z";
+        LOGGER.debug("input="+input);
+        String output = ResolvePath.replaceVars(input);
+        LOGGER.debug("output="+output);
+        assert (!output.contains("Unknown"));
     }
 }
