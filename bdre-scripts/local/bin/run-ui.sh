f83923b89b54e5e1e2cca7d8ebe55b713a14cedd
Arijit Banerjee <arijit_banerjee1@wipro.com>
2016-01-09 01:55:35 +0530

    fixing bdre service

--- bdre-scripts/local/bin/run-ui.sh
+++ bdre-scripts/local/bin/run-ui.sh
@@ -3,11 +3,8 @@
 . $(dirname $0)/../env.properties
 BDRE_HOME=~/bdre
 BDRE_APPS_HOME=~/bdre_apps
-LOG_FILE_OPTION="--out $1"
-if [ -z "$1" ]; then
-        echo Log file path not provided.
-        LOG_FILE_OPTION=""
-fi
+
 echo "writing logs to $1"
 
-java -jar  $BDRE_HOME/lib/webapps/jetty-runner.jar $LOG_FILE_OPTION --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui-$bdreVersion.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war
+java -jar  $BDRE_HOME/lib/webapps/jetty-runner.jar -o $1 --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui-$bdreVersion.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war &
+echo $! > $2
