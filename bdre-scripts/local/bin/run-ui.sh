6abe28a8dae5d9830060b1ea104ad67565a512e5
Arijit Banerjee <arijit_banerjee1@wipro.com>
2016-01-09 02:04:08 +0530

    fixing bdre service

--- bdre-scripts/local/bin/run-ui.sh
+++ bdre-scripts/local/bin/run-ui.sh
@@ -4,7 +4,7 @@
 BDRE_HOME=~/bdre
 BDRE_APPS_HOME=~/bdre_apps
 
-echo "writing logs to $1 and pid is $2"
+echo "writing logs to $1 and pid file is $2"
 
 java -jar  $BDRE_HOME/lib/webapps/jetty-runner.jar --out $1 --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui-$bdreVersion.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war &
 echo $! > $2
