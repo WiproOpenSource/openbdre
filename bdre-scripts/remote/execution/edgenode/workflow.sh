293135db4f496596d7ed39cfe955897da680dd8d
Farooque <mohammad.siddiqui5@wipro.com>
2016-01-28 04:49:11 -0800

    added tiled to execute variable value

--- bdre-scripts/remote/execution/edgenode/workflow.sh
+++ bdre-scripts/remote/execution/edgenode/workflow.sh
@@ -6,6 +6,9 @@ BDRE_APPS_HOME=~/bdre_apps
 BDRE_REMOTE_HOME=~$edgeNodeUserName/bdre
 EDGE_NODE_USER_NAME=$edgeNodeUserName
 EDGE_NODE_PASSWORD=$edgeNodePassword
+OOZIE_HOST=$oozieHost
+OOZIE_PORT=$ooziePort
+OOZIE_URL=$oozieUrl
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
         echo Insufficient parameters !
@@ -16,4 +19,4 @@ busDomainId=$1
 processTypeId=$2
 processId=$3
 
-curl -i -k -u $EDGE_NODE_USER_NAME:$EDGE_NODE_PASSWORD -H Content-Type:application/xml -T $BDRE_APPS_HOME/$busDomainId/$processTypeId/$processId/job-$processId.xml -X POST 'https://$oozieHost:$ooziePort$oozieUrl/v1/jobs?action=start' > $logDir/oozie-$processId.log 2> $logDir/oozie-error-$processId.log
+curl -i -k -u $EDGE_NODE_USER_NAME:$EDGE_NODE_PASSWORD -H Content-Type:application/xml -T $BDRE_APPS_HOME/$busDomainId/$processTypeId/$processId/job-$processId.xml -X POST 'https://`$OOZIE_HOST`:`$OOZIE_PORT``$OOZIE_URL`/v1/jobs?action=start' > $logDir/oozie-$processId.log 2> $logDir/oozie-error-$processId.log
