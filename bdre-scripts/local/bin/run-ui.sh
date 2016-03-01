368597aefcb35a5ee696958e8349550eaff037e5
bdreprem <prem.kumar51@wipro.com>
2016-02-29 22:38:32 -0800

    PermGen space error while starting BDRE service in CDH VM BDRE-89

--- bdre-scripts/local/bin/run-ui.sh
+++ bdre-scripts/local/bin/run-ui.sh
@@ -6,5 +6,5 @@ BDRE_APPS_HOME=~/bdre_apps
 
 echo "writing logs to $1 and pid file is $2"
 
-java -jar  -XX:MaxPermSize=2048m $BDRE_HOME/lib/webapps/jetty-runner.jar --out $1 --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui-$bdreVersion.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war --path /store $BDRE_HOME/bdre-scripts/appstore-context.xml &
+java -jar -XX:MaxPermSize=2048m $BDRE_HOME/lib/webapps/jetty-runner.jar --out $1 --port 28850 --path /mdui $BDRE_HOME/lib/webapps/md-ui-$bdreVersion.war --path /mdrest $BDRE_HOME/lib/webapps/md-rest-api-$bdreVersion.war --path /auth  $BDRE_HOME/lib/webapps/auth-rest-api-$bdreVersion.war --path /store $BDRE_HOME/bdre-scripts/appstore-context.xml &
 echo $! > $2
