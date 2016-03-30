ec1ab0d5cd3d9f3d00cc67d954639a7f65c460c7
Sudam Madhav Sagar <sudam.sagar@wipro.com>
2016-03-30 14:12:29 +0530

    bdre-245 adding mdrestapi for hivemigration

---
+++ md-rest-api/src/test/java/com/wipro/ats/bdre/md/rest/ext/HiveTableMigrationAPITest.java
@@ -0,0 +1,37 @@
+package com.wipro.ats.bdre.md.rest.ext;
+
+import com.wipro.ats.bdre.md.rest.RestWrapper;
+import com.wipro.ats.bdre.md.rest.RestWrapperOptions;
+import org.apache.log4j.Logger;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by SU324335 on 3/29/2016.
+ */
+public class HiveTableMigrationAPITest {
+    private static final Logger LOGGER = Logger.getLogger(HiveTableMigrationAPITest.class);
+
+    @Test
+
+    public void getDBList()
+    {
+        HiveTableMigrationAPI hiveTableMigrationAPI = new HiveTableMigrationAPI();
+        RestWrapperOptions restWrapperOptions = hiveTableMigrationAPI.getDBList();
+        LOGGER.info(restWrapperOptions.getOptions().toString());
+    }
+
+    @Test
+
+    public void getTablesList()
+    {
+        HiveTableMigrationAPI hiveTableMigrationAPI = new HiveTableMigrationAPI();
+        Map<String,String> map = new TreeMap<String, String>();
+        map.put("srcDB_default","default");
+        RestWrapperOptions restWrapperOptions = hiveTableMigrationAPI.getTableList(map);
+        LOGGER.info(restWrapperOptions.getOptions().toString());
+    }
+}
