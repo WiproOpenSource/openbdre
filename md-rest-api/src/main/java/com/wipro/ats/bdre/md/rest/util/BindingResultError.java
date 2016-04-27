ef41b3b895d21fbb0ef6a3a9afac065f05d6d02b
mishisinku <mishi.sinku@wipro.com>
2016-04-27 05:44:34 -0700

    removing unnecessary <p> tag BDRE-190

--- md-rest-api/src/main/java/com/wipro/ats/bdre/md/rest/util/BindingResultError.java
+++ md-rest-api/src/main/java/com/wipro/ats/bdre/md/rest/util/BindingResultError.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class BindingResultError {
     public RestWrapper errorMessage(BindingResult bindingResult){
         RestWrapper restWrapper = null;
-        StringBuilder errorMessages = new StringBuilder("<p>Please fix following errors and try again<p><ul>");
+        StringBuilder errorMessages = new StringBuilder("<p>Please fix following errors and try again</p><ul>");
         List<FieldError> errors = bindingResult.getFieldErrors();
         for (FieldError error : errors) {
             errorMessages.append("<li>");
