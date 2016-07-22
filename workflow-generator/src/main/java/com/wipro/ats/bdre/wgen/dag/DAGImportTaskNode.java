bc08c3d7d649edba426d6fbf2d042ddd5158e84f
mishisinku <mishi.sinku@wipro.com>
2016-07-22 02:32:40 -0700

    Adding import task node BDRE-431

--- workflow-generator/src/main/java/com/wipro/ats/bdre/wgen/dag/DAGImportTaskNode.java
+++ workflow-generator/src/main/java/com/wipro/ats/bdre/wgen/dag/DAGImportTaskNode.java
@@ -1,7 +1,98 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wipro.ats.bdre.wgen.dag;
 
+import com.wipro.ats.bdre.MDConfig;
+import com.wipro.ats.bdre.md.beans.ProcessInfo;
+import org.apache.log4j.Logger;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
- * Created by cloudera on 7/21/16.
+ * Created by IshitaParekh on 04-03-2015.
  */
-public class DAGImportTaskNode {
+public class DAGImportTaskNode extends GenericActionNode {
+    private static final Logger LOGGER = Logger.getLogger(DAGImportTaskNode.class);
+
+    private ProcessInfo processInfo = new ProcessInfo();
+    private DAGTaskNode taskNode = null;
+
+    /**
+     * This constructor is used to set node id and process information.
+     *
+     * @param taskNode An instance of ActionNode class which a workflow triggers the execution of a task.
+     */
+    public DAGImportTaskNode(DAGTaskNode taskNode) {
+        setId(taskNode.getId());
+        processInfo = taskNode.getProcessInfo();
+        this.taskNode = taskNode;
+    }
+
+    public ProcessInfo getProcessInfo() {
+        return processInfo;
+    }
+
+
+    public String getName() {
+
+        String nodeName = "import-" + getId() + "-" + processInfo.getProcessName().replace(' ', '_');
+        return nodeName.substring(0, Math.min(nodeName.length(), 45));
+
+    }
+
+
+    @Override
+    public String getDAG() {
+        if (this.getProcessInfo().getParentProcessId() == 0) {
+            return "";
+        }
+        String homeDir = System.getProperty("user.home");
+        StringBuilder ret = new StringBuilder();
+        ret.append("\ndef "+ getName().replace('-','_')+"_pc():\n" +
+                "\tcommand='java -cp "+homeDir+"/bdre/lib/md_api/md_api-1.1-SNAPSHOT-executable.jar:"+homeDir+"/bdre/lib/*/*  com.wipro.ats.bdre.dataimport.DataImportMain --process-id "+ getId().toString()+"--batch-id  dict[\"initJobInfo.getTargetBatchId()\"]"+ "--config-group imp-common"+"--instance-exec-id dict[\"initJobInfo.getInstanceExecId()\"] ' \n"+
+                "\tbash_output = subprocess.Popen(command,shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE )\n" +
+                "\tout,err = bash_output.communicate()\n"+
+                "\tprint(\"out is \",out)\n"+
+                "\tprint(\"err is \",err)\n"+
+                "\tif(bash_output.returncode > 0):\n" +
+                "\t\treturn 'dummy_"+getName().replace('-', '_') +"'\n" +
+                "\telse:\n" +
+                "\t\treturn '"+getToNode().getName().replace('-', '_') +"'\n" +
+
+                "\ndef f_"+ getName().replace('-','_')+"():\n" +
+                "\t"+ getName().replace('-', '_')+".set_downstream("+ getToNode().getName().replace('-', '_')+")\n" +
+                "\t"+ getName().replace('-', '_')+".set_downstream(dummy_"+ getName().replace('-', '_')+")\n" +
+                "\t"+ "dummy_"+ getName().replace('-', '_')+".set_downstream("+getTermNode().getName().replace('-', '_') +")\n"+
+                getName().replace('-','_')+" = BranchPythonOperator(task_id='"+getName().replace('-', '_')+"', python_callable="+getName().replace('-','_')+"_pc, dag=dag)\n"+
+                "dummy_"+ getName().replace('-', '_')+" = DummyOperator(task_id ='"+"dummy_"+ getName().replace('-', '_')+"',dag=dag)\n"
+        );
+
+
+        try {
+
+            FileWriter fw = new FileWriter(homeDir+"/defFile.txt", true);
+            fw.write("\nf_"+getName().replace('-', '_')+"()");
+            fw.close();
+        }
+        catch (IOException e){
+            LOGGER.debug("error occured:  " + e);
+        }
+
+
+        return  ret.toString();
+
+    }
 }
