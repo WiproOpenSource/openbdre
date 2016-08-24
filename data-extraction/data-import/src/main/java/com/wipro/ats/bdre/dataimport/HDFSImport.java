c8266337143b4b096730e8dbebc7fcc3385d1f3e
Madhav <sudam.sagar@wipro.com>
2016-08-24 05:10:46 -0700

    solved capturing : in path,creationTS of fileinfo

--- data-extraction/data-import/src/main/java/com/wipro/ats/bdre/dataimport/HDFSImport.java
+++ data-extraction/data-import/src/main/java/com/wipro/ats/bdre/dataimport/HDFSImport.java
@@ -223,51 +223,51 @@ public class HDFSImport extends Configured implements Tool {
                         BufferedWriter bw = new BufferedWriter(fw);
 
                         if(registerFileInfo.getSubProcessId() != null)
-                            bw.write("fileInfo.getSubProcessId():"+registerFileInfo.getSubProcessId().toString()+"\n");
+                            bw.write("fileInfo.getSubProcessId()::"+registerFileInfo.getSubProcessId().toString()+"\n");
                         else
-                            bw.write("fileInfo.getSubProcessId():null\n");
+                            bw.write("fileInfo.getSubProcessId()::null\n");
 
                         if(registerFileInfo.getServerId() != null)
-                            bw.write("fileInfo.getServerId():"+registerFileInfo.getServerId().toString()+"\n");
+                            bw.write("fileInfo.getServerId()::"+registerFileInfo.getServerId().toString()+"\n");
                         else
-                            bw.write("fileInfo.getServerId():null\n");
+                            bw.write("fileInfo.getServerId()::null\n");
 
                         if(registerFileInfo.getPath() != null)
-                            bw.write("fileInfo.getPath():"+registerFileInfo.getPath()+"\n");
+                            bw.write("fileInfo.getPath()::"+registerFileInfo.getPath()+"\n");
                         else
-                            bw.write("fileInfo.getPath():null\n");
+                            bw.write("fileInfo.getPath()::null\n");
 
                         if(registerFileInfo.getFileSize() != null)
-                            bw.write("fileInfo.getFileSize():"+registerFileInfo.getFileSize().toString()+"\n");
+                            bw.write("fileInfo.getFileSize()::"+registerFileInfo.getFileSize().toString()+"\n");
                         else
-                            bw.write("fileInfo.getFileSize():null\n");
+                            bw.write("fileInfo.getFileSize()::null\n");
 
                         if(registerFileInfo.getFileHash() != null)
-                            bw.write("fileInfo.getFileHash():"+registerFileInfo.getFileHash().toString()+"\n");
+                            bw.write("fileInfo.getFileHash()::"+registerFileInfo.getFileHash().toString()+"\n");
                         else
-                            bw.write("fileInfo.getFileHash():null\n");
+                            bw.write("fileInfo.getFileHash()::null\n");
 
                         if(registerFileInfo.getCreationTs() != null) {
-                            String creationTs = registerFileInfo.getCreationTs().toString().replace(" ", "__").replace(":", "zzzz");//Recovered back in RegisterFile.java CreationTs has space(which splits parameter) and :(creates great problem while creating python dictionaries)
+                            String creationTs = registerFileInfo.getCreationTs().toString().replace(" ", "__");//Recovered back in RegisterFile.java CreationTs has space(which splits parameter) and :(creates great problem while creating python dictionaries)
                             LOGGER.info("Creation Ts modified is "+creationTs);
-                            bw.write("fileInfo.getCreationTs():" + creationTs + "\n");
+                            bw.write("fileInfo.getCreationTs()::" + creationTs + "\n");
                         }else
-                            bw.write("fileInfo.getCreationTs():null\n");
+                            bw.write("fileInfo.getCreationTs()::null\n");
 
                         if(registerFileInfo.getBatchId() != null)
-                            bw.write("fileInfo.getBatchId():"+registerFileInfo.getBatchId().toString()+"\n");
+                            bw.write("fileInfo.getBatchId()::"+registerFileInfo.getBatchId().toString()+"\n");
                         else
-                            bw.write("fileInfo.getBatchId():null\n");
+                            bw.write("fileInfo.getBatchId()::null\n");
 
                         if(registerFileInfo.getParentProcessId() != null)
-                            bw.write("fileInfo.getParentProcessId():"+registerFileInfo.getParentProcessId().toString()+"\n");
+                            bw.write("fileInfo.getParentProcessId()::"+registerFileInfo.getParentProcessId().toString()+"\n");
                         else
-                            bw.write("fileInfo.getParentProcessId():null\n");
+                            bw.write("fileInfo.getParentProcessId()::null\n");
 
                         if(registerFileInfo.getBatchMarking() != null)
-                            bw.write("fileInfo.getBatchMarking():"+registerFileInfo.getBatchMarking()+"\n");
+                            bw.write("fileInfo.getBatchMarking()::"+registerFileInfo.getBatchMarking()+"\n");
                         else
-                            bw.write("fileInfo.getBatchMarking():null\n");
+                            bw.write("fileInfo.getBatchMarking()::null\n");
 
                         bw.close();
 
--- workflow-generator/src/main/java/com/wipro/ats/bdre/wgen/dag/DAGFileRegistrationTaskNode.java
+++ workflow-generator/src/main/java/com/wipro/ats/bdre/wgen/dag/DAGFileRegistrationTaskNode.java
@@ -67,7 +67,7 @@ public class DAGFileRegistrationTaskNode extends GenericActionNode {
         ret.append(
                 "with open('"+jobInfoFile+"','a+') as propeties_register_file:\n"+
                 "\tfor line in propeties_register_file:\n"+
-                "\t\tfile_info = line.split(':',2)\n"+
+                "\t\tfile_info = line.split('::',2)\n"+
                 "\t\tdict[file_info[0]] = file_info[1].replace('\\n','')\n"+
 
                 "\ndef "+ getName().replace('-','_')+"_pc():\n" +
