ef302ec681d71c3ce1e44e8619dd4529f33e8eef
Prem Kumar <prem.kumar51@wipro.com>
2017-11-30 03:13:23 -0800

    merged with sourav code

--- spark-streaming/src/main/java/analytics/LogisticRegression.java
+++ spark-streaming/src/main/java/analytics/LogisticRegression.java
@@ -6,7 +6,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.ml.Pipeline;
-import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.classification.LogisticRegressionModel;
 import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.StringIndexerModel;
